55a112199dac5cc72782033f48038ff9086ada2c
chelseamariepeterson <peterson.1166@osu.edu>
2021-07-15 16:21:12 +0000

    first commit

--- Example1/app.R
+++ Example1/app.R
@@ -13,37 +13,106 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Scatter and Linear Model Plots - 7030"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            # Input: Select a file ----
+            fileInput("file1", "Choose CSV File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Checkbox if file has header ----
+            checkboxInput("header", "Header", TRUE),
+            
+            # Input: Select separator ----
+            radioButtons("sep", "Separator",
+                         choices = c(Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"),
+                         selected = ","),
+            
+            # Input: Select quotes ----
+            radioButtons("quote", "Quote",
+                         choices = c(None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"),
+                         selected = '"'),
+            
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Select number of rows to display ----
+            radioButtons("disp", "Display",
+                         choices = c(Head = "head",
+                                     All = "all"),
+                         selected = "head")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            plotOutput("distPlot"),
+            plotOutput("distLine"),
+            plotOutput("contents"),
+            plotOutput("lmPlot"),
+            plotOutput("equation")
+            
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Define server logic required to draw a histogram 
+ server <- function(input, output) {
+    dataInput <- reactive({
+        req(input$file1)
+        
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+        return(df)
     })
-}
-
+    
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #     
+    #     # draw the histogram with the specified number of bins
+    #     hist(x,
+    #          breaks = bins,
+    #          col = 'darkgray',
+    # #          border = 'white')
+    # })
+    
+    output$distPlot <- renderPlot({
+        plot(dataInput()$x,dataInput()$y)
+    })
+    
+    output$lmtPlot <- renderPlot({
+        plot(dataInput()$x,dataInput()$y)
+    })
+    
+    output$contents <- renderTable({
+        
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
+        
+        
+        if(input$disp == "head") {
+            return(head(dataInput()))
+        }
+        else {
+            return(dataInput())
+        }
+        
+    })
+    
 # Run the application 
-shinyApp(ui = ui, server = server)
+ shinyApp(ui = ui, server = server)
