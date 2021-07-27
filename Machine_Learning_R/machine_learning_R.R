0f1d1ecdff96d76cd8799bea073761f9c479c0f1
chelseamariepeterson <peterson.1166@osu.edu>
2021-07-27 16:14:44 +0000

    second commit

--- Machine_Learning_R/machine_learning_R.R
+++ Machine_Learning_R/machine_learning_R.R
@@ -1,3 +1,9 @@
+library(caret)
+install.packages("ellipse")
+install.packages("kernlab")
+install.packages("e1071")
+install.packages("randomForest")
+
 # attach the iris dataset to the environment
 data(iris)
 # rename the dataset
@@ -25,4 +31,74 @@ levels(dataset$Species)
 # summarize the class distribution
 percentage <- prop.table(table(dataset$Species)) * 100
 cbind(freq=table(dataset$Species), percentage=percentage)
+
+# summarize attribute distributions
+summary(dataset)
+
+##Univariate Plots (plots of each variable)
+# split input and output
+
+x <- dataset[,1:4]
+y <- dataset[,5]
+
+##Boxplots
+# boxplot for each attribute on one image
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  boxplot(x[,i], main=names(iris)[i])
+}
+
+##BarPlots
+# barplot for class breakdown
+plot(y)
+#Bar Plot of Iris Flower Species
+
+install.packages("ellipse")
+
+##Multivariate Plots (interactions between variables)
+# scatterplot matrix
+featurePlot(x=x, y=y, plot="ellipse")
+
+# box and whisker plots for each attribute
+featurePlot(x=x, y=y, plot="box")
+
+# density plots for each attribute by class value
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+##5. Evaluate Some Algorithms
+###We will 10-fold crossvalidation to estimate accuracy.
+# Run algorithms using 10-fold cross validation
+control <- trainControl(method="cv", number=10)
+metric <- "Accuracy"
+
+##Let’s evaluate 5 different algorithms:
+  
+###Linear Discriminant Analysis (LDA)
+###Classification and Regression Trees (CART).
+###k-Nearest Neighbors (kNN).
+###Support Vector Machines (SVM) with a linear kernel.
+###Random Forest (RF)
+
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+
+# summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
 
